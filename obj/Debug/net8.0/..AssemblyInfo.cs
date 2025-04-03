bfd7270dfabe51324463b3710b51135ebd537a45
rex-makusia <grecpt@gmail.com>
2025-04-04 05:37:27 +1000

    Building the project

--- obj/Debug/net8.0/..AssemblyInfo.cs
+++ obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085130de62249ed05e7991a782421c46787b523a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb5ad4abdea577e7e3923ecc664912de5c864f5")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
